82385ef203bd9b3fb869a46e8469e753bb719201
Fluch2001 <fluchtenberg14@gmail.com>
2025-06-03 22:07:41 -0300

    done & delete function added

--- script.js
+++ script.js
@@ -2,35 +2,74 @@ const date = document.querySelector('#date');
 const list = document.querySelector('#list');
 const input = document.querySelector('#input');
 const enterButton = document.querySelector('#enter');
+const check = 'fa-check-circle';
+const uncheck = 'fa-circle';
+const lineThrough = 'line-through';
+let id = 0;
 
 // fijarse que va 
 console.log(date);
 
 //Add task function
 
-function addTask (addTask) {
+function addTask (taskText, id, done, deleted) {
+
+    if(deleted){return} 
+
+    const DONE = done ? `fas ${check}` : `far ${uncheck}`;
+    const LINE = done ?lineThrough :''
+
     const addToList =  `<li id="addTask">
-                        <i class="far fa-circle co" data="done" id="0"></i>
-                        <p class="text">${addTask}</p>
-                        <i class="fas fa-trash de" data="deleted" id="0"></i>
+                        <i class="${DONE}" data="done" id="${id}"></i>
+                        <p class="text ${LINE}">${taskText}</p>
+                        <i class="fas fa-trash de" data="deleted" id="${id}"></i>
                         </li>`
     list.insertAdjacentHTML("beforeend",addToList);   
 }
 
+// Done task function
+
+function doneTask(element){
+    element.classList.toggle(check);
+    element.classList.toggle(uncheck);
+    element.parentNode.querySelector('.text').classList.toggle(lineThrough)
+}
+
+// Deleted task function
+
+function deletedTask(element) {
+    element.parentNode.parentNode.removeChild(element.parentNode)
+}
+
+
 enterButton.addEventListener('click', () => {
     const taskText = input.value;
     if (taskText) {
-        addTask(taskText);
+        addTask(taskText, id, false, false);
     }
     input.value = '';
+    id++;
 });
 
 document.addEventListener('keyup', function(event) {
     if (event.key === 'Enter') {
         const taskText = input.value;
         if (taskText) {
-            addTask(taskText);
+            addTask(taskText, id, false, false);
         }
         input.value = '';
+        id++;
+    }
+});
+
+list.addEventListener('click',function(event){
+
+    const element = event.target;
+    const elementData = element.attributes.data.value;
+    if(elementData === 'done'){
+        doneTask(element);
+    }
+    else if (elementData === 'deleted'){
+        deletedTask(element);
     }
 });
